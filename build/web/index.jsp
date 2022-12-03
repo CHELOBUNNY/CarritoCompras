f17889135000f0d5314eac63674d17c5ba93453f
Marcelo Fernando Puna Tarqui <chelo.bunny@gmail.com>
2022-12-03 14:19:13 -0400

    ProyectoEmergentes

---
+++ build/web/index.jsp
@@ -0,0 +1,174 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>   
+        <link rel="stylesheet" type="text/css" href="fond.css">
+
+        
+        <title>Carrito de Compras</title>
+
+    <body>
+       
+                
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#"><i></i></a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                 
+                    <li class="nav-item">
+                        <a  class="nav-link" href="./Controlador?accion=home"><i class="fas fa-plus-circle"></i><label style="color:#ffdf00"> Ofertas del Dia</a>
+                    </li>                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Controlador?accion=carrito"><i class="fas fa-cart-plus">(<label style="color:#ffdf00">${cont}</label>)</i> Carrito</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="./Controlador?accion=NuevoProducto"><label style="color:#de4c8a">Productos</a>
+                    </li> 
+                </ul>
+                    
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <!--<form class="form-inline my-2 my-lg-0">-->
+                    <input style="width:200px" class="form-control mr-sm-2" id="txtBuscar">
+                    <button class="btn btn-outline-info my-2 my-sm-0" id="btnBuscar"><i class="fas fa-search"></i> Buscar</button>
+                    <!-- </form>       -->                 
+                </ul>                                
+                  
+            </div>
+                            
+
+        </nav>
+              <section class="showcase">
+    <header>
+      <h2 class="logo"></h2>
+      
+    </header>           
+    
+         <video src="vd/vd/FD.mp4" muted loop autoplay></video>
+          <div class="overlay"></div>
+    <div class="text">
+      
+      <a href="Menuvista.html">Menu</a>
+
+         
+
+                                              
+    <div class="overlay">
+            </div>
+        <div class="container mt-4">
+            <div class="row">
+              
+                <c:forEach var="p" items="${productos}">
+                    
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <div class="card">
+                                <div class="card-header">
+                                    <label class="col-sm-12"><label style="color:#ffdf00">${p.getNombres()}</label>                                    
+                                </div>
+                               
+                                <div class="card-body text-center d-flex">
+                                    <label><i class="fas fa-dollar-sign">${p.getPrecio()}</i></label>
+                                    <img src="${p.getImagen()}" width="200" height="170">
+                                </div>
+                                <div class="card-footer">
+                                    <div class="col-sm-12">
+                                        <label>${p.getDescripcion()}</label>                                   
+                                    </div>
+                                    <div class=" col-sm-12 text-center">                                
+                                        <a href="Controlador?accion=AgregarCarrito&id=${p.getId()}" class="btn btn2 btn-outline-primary">Agregar a Carrito<i class="fas fa-cart-plus"></i></a>
+                                        <a href="Controlador?accion=Comprar&id=${p.getId()}" class="btn btn-danger">Comprar</a>
+                                    </div>                         
+                                </div>
+                            </div>
+                                    
+                        </div>
+                                    
+                    </div>
+                                    
+                </c:forEach>
+                
+            </div>
+              </div>  
+    
+    </div>
+      
+              
+        <!-- Modal Iniciar Session o Registrarse -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="container col-lg-9">                   
+                    <div class="modal-content">                   
+                        <div class="pr-2 pt-1">                         
+                            <button type="button" class="close" data-dismiss="modal">
+                                <span aria-hidden="true">&times;</span>
+                            </button>                    
+                        </div>
+                        <div class="text-center">                         
+                            <img src="img/user.png" width="100" height="100">                         
+                        </div>
+
+                        <div class="modal-header text-center">                      
+                            <ul class="nav nav-pills">                           
+                                <li class="nav-item">
+                                    <a class="nav-link active" data-toggle="pill" href="#pills-iniciar">Iniciar Sesion</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-toggle="pill" href="#pills-registrar">Registrarse</a>
+                                </li>                          
+                            </ul>  
+                        </div>
+                        <div class="modal-body"> 
+                            <div class="tab-content" id="pills-tabContent">
+                                <!-- Iniciar Session -->
+                              
+                                <!-- Registrarse -->
+                                <div class="tab-pane fade" id="pills-registrar" role="tabpanel">
+                                    <form action="Controlador">                               
+                                        <div class="form-group">
+                                            <label>Nombres</label>
+                                            <input type="text" name="txtnom" class="form-control" placeholder="NAME">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Dni</label>
+                                            <input type="text" maxlength="8" name="txtdni" class="form-control" placeholder="01245678">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Direccion</label>
+                                            <input type="text" name="txtdire" class="form-control" placeholder="DIRECCION">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Email address</label>
+                                            <input type="email" name="txtemail" class="form-control" placeholder="email@example.com">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Password</label>
+                                            <input type="password" name="txtpass" class="form-control" placeholder="Password">
+                                        </div>                                  
+                                        <button type="submit" name="accion" value="Registrar" class="btn btn-outline-danger btn-block">Crear Cuenta</button>
+                                    </form>
+                                </div>                          
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+               
+    </div>
+              </section>
+          
+    </body>
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="js/index.js" type="text/javascript"></script>
+        <link href="fond/fond.css" rel="stylesheet" type="text/css"/>
+    </body>
+</html>
---
+++ web/products.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Productos</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Course Project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/courses_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/courses_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header d-flex flex-row">
+		<div class="header_content d-flex flex-row align-items-center">
+			<!-- Logo -->
+			<div class="logo_container">
+				<div class="logo">
+					<img src="images/logo.png" alt="">
+					<span>qhapap-takesi</span>
+				</div>
+			</div>
+
+			<!-- Main Navigation -->
+			<nav class="main_nav_container">
+				<div class="main_nav">
+					<ul class="main_nav_list">
+						<li class="main_nav_item"><a href="Menuvista.html">INICIO</a></li>
+						<li class="main_nav_item"><a href="Menuvista.html">PRODUCTOS</a></li>
+						<li class="main_nav_item"><a href="Menuvista.html">PORTAFOLIO</a></li>
+						<li class="main_nav_item"><a href="index.jsp">LOGIN</a></li>
+						<li class="main_nav_item"><a href="Menuvista.html">CONTACTO</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<div class="header_side d-flex flex-row justify-content-center align-items-center">
+			<img src="images/phone-call.svg" alt="">
+			<span>+591 73228969</span>
+		</div>
+
+		<!-- Hamburger -->
+		<div class="hamburger_container">
+			<i class="fas fa-bars trans_200"></i>
+		</div>
+
+	</header>
+	
+	<!-- Menu -->
+	<div class="menu_container menu_mm">
+
+		<!-- Menu Close Button -->
+		<div class="menu_close_container">
+			<div class="menu_close"></div>
+		</div>
+
+		<!-- Menu Items -->
+		<div class="menu_inner menu_mm">
+			<div class="menu menu_mm">
+				<ul class="menu_list menu_mm">
+					<li class="main_nav_item"><a href="index.php">INICIO</a></li>
+						<li class="main_nav_item"><a href="Menuvista.html">PRODUCTOS</a></li>
+						<li class="main_nav_item"><a href="Menuvista.html">PORTAFOLIO</a></li>
+						<li class="main_nav_item"><a href="index.jsp">LOGIN</a></li>
+				</ul>
+
+				<!-- Menu Social -->
+				
+				<div class="menu_social_container menu_mm">
+					<ul class="menu_social menu_mm">
+						
+						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+						<li class="menu_social_item"><a href="https://chat.whatsapp.com/HVA1MLhBAOG6nuZxUy8HT7"><i class="fab fa-whatsapp"></i></a></li>
+					</ul>
+				</div>
+
+				<div class="menu_copyright menu_mm"></div>
+			</div>
+
+		</div>
+
+	</div>
+	
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background_container prlx_parent">
+			<div class="home_background prlx" style="background-image:url(images/courses_background.jpg)"></div>
+		</div>
+		<div class="home_content">
+			<h1>PRODUCTOS</h1>
+		</div>
+	</div>
+
+	<!-- Popular -->
+
+	<div class="popular page_section">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title text-center">
+						<h1>LOS MAS POPULARES </h1>
+					</div>
+				</div>
+			</div>
+
+			<div class="row course_boxes">
+				
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_11.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Heridas,Alergias,Reduce la colesterol,Fiebre,resfriadsos,Salud.etc</div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">MIEL <span> NATURAL</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$11.58</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_12.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Heridas,Resfriados,Alergias,Dolores,Tratamiento contra COVID-19,Asma,Otitis,Laringitis,etc.</div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">PROPÒLEO <span> NATURAL</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$6,49</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_13.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Neumonìa,Verrugas,Herpes,Fiebre,Tratamiento contra COVID-19,etc. </div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">PROPOMIEL, <span> NATURAL</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$11.53</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_14.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Energias,Contra la Diabitis,Rejuvenecimiento,Concentraciòn,etc,</div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">JALEA <span> REAL</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$28.80</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_15.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Contiene 10 marcos planta baja con abeja </div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">CAJA  <span> PLANTA BAJA</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$123</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_16.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="Menuvista.html">COMPRAR</a></div>
+							<div class="card-text">Contiene 20 marcos planta baja y alsa con abeja </div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name"> CAJON <span> CON ALSA</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$261</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_17.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="contact.php">COMPRAR</a></div>
+							<div class="card-text">Contiene 10 marcos planta baja y la alsa contiene 10 medio marco con abeja </div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">CAJON  <span>CON MEDIA ALSA</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$221</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+				<div class="col-lg-4 course_box">
+					<div class="card">
+						<img class="card-img-top" src="images/course_18.jpg" alt="">
+						<div class="card-body text-center">
+							<div class="card-title"><a href="contact.php">COMPRAR</a></div>
+							<div class="card-text">Contiene 4 marcos 1 de alimento y resto crias con abeja</div>
+						</div>
+						<div class="price_box d-flex flex-row align-items-center">
+							<div class="course_author_image">
+								<img src="images/author.jpg" alt="">
+							</div>
+							<div class="course_author_name">NÙCLEO <span> COMPLETO</span></div>
+							<div class="course_price d-flex flex-column align-items-center justify-content-center"><span>$65</span></div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Popular Course Item -->
+			</div>
+		</div>		
+	</div>
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="container">
+			
+			<!-- Newsletter -->
+
+
+			<!-- Footer Content -->
+
+			<div class="footer_content">
+				<div class="row">
+
+					<!-- Footer Column - About -->
+					<div class="col-lg-3 footer_col">
+
+						<!-- Logo -->
+						<div class="logo_container">
+							<div class="logo">
+								<img src="images/logo.png" alt="">
+								<span>qhapap-takesi</span>
+							</div>
+						</div>
+
+						<p class="footer_about_text">Bienvenido a la ùnica plataforma que ofrece productos de remate y te ofrece una amplia experiencia del tema como bellas fotografias de turismo para que lo vivas.</p>
+
+					</div>
+
+					<!-- Footer Column - Menu -->
+
+					<div class="col-lg-3 footer_col">
+						<div class="footer_column_title"></div>
+						<div class="footer_column_content">
+							<ul>
+							</ul>
+						</div>
+					</div>
+
+					<!-- Footer Column - Usefull Links -->
+
+					<div class="col-lg-3 footer_col">
+						<div class="footer_column_title">Menu</div>
+						<div class="footer_column_content">
+							<ul>
+								
+							</ul>
+						</div>
+					</div>
+
+					<!-- Footer Column - Contact -->
+
+					<div class="col-lg-3 footer_col">
+						<div class="footer_column_title">Contacto</div>
+						<div class="footer_column_content">
+							<ul>
+								<li class="footer_contact_item">
+									<div class="footer_contact_icon">
+										<img src="images/placeholder.svg" href="https://www.google.com/maps/place/-16.396689,-67.722698/@-16.3967861,-67.7048037,10z/data=!4m2!3m1!1s0">
+									</div>
+									Yanacachi Sud Yungas
+								</li>
+								<li class="footer_contact_item">
+									<div class="footer_contact_icon">
+										<img src="images/smartphone.svg" alt="https://www.google.com/maps/place/-16.396689,-67.722698/@-16.3967861,-67.7048037,10z/data=!4m2!3m1!1s0">
+									</div>
+									+591-73228969
+								</li>
+								<li class="footer_contact_item">
+									<div class="footer_contact_icon">
+										<img src="images/envelope.svg" alt="">
+									</div>takesqhapaqnan1@gmail.com
+								</li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<!-- Footer Copyright -->
+
+			<div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
+				<div class="footer_copyright">
+					<span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+             <script>document.write(new Date().getFullYear());</script>Derechos | Reservados <i class="fa fa-heart" aria-hidden="true"></i> 2022 <a href="https://www.google.com/maps/place/-16.396689,-67.722698/@-16.3967861,-67.7048037,10z/data=!4m2!3m1!1s0" target="_blank">qhapap-takesi</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
+				</div>
+				<div class="footer_social ml-sm-auto">
+					<ul class="menu_social">
+						<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+						<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+						<li class="menu_social_item"><a href="https://chat.whatsapp.com/HVA1MLhBAOG6nuZxUy8HT7"><i class="fab fa-whatsapp"></i></a></li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/scrollTo/jquery.scrollTo.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
